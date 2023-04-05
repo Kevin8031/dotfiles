c84db6799542370c6fcfd0383da775e3175f6187
Kevin8031 <concilio.kevin@gmail.com>
2023-04-05 23:16:42 +0200

    fix bufferline shortcut for cycling

--- nvim/.config/nvim/after/plugin/bufferline.lua
+++ nvim/.config/nvim/after/plugin/bufferline.lua
@@ -10,7 +10,7 @@ require("bufferline").setup {
 vim.keymap.set("n", "<A-h>", "<cmd>BufferLineMovePrev<cr>", opts)
 vim.keymap.set("n", "<A-l>", "<cmd>BufferLineMoveNext<cr>", opts)
 
-vim.keymap.set("n", "<C-h>", "<cmd>bNext<cr>", opts)
-vim.keymap.set("n", "<C-l>", "<cmd>bnext<cr>", opts)
+vim.keymap.set("n", "<C-h>", "<cmd>BufferLineCyclePrev<cr>", opts)
+vim.keymap.set("n", "<C-l>", "<cmd>BufferLineCycleNext<cr>", opts)
 
 vim.keymap.set("n", "<C-j>", "<cmd>bdelete<cr>", opts)
