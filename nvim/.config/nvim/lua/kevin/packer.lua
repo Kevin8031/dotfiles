05d84411930e135f903e6f7da9ea3b1ce37b9d31
Kevin8031 <concilio.kevin@proton.me>
2023-06-05 21:09:35 +0200

    added MarkdownPreview plugin

--- nvim/.config/nvim/lua/kevin/packer.lua
+++ nvim/.config/nvim/lua/kevin/packer.lua
@@ -77,4 +77,7 @@ return require('packer').startup(function(use)
     -- Debugging
     use 'nvim-lua/plenary.nvim'
     use 'mfussenegger/nvim-dap'
+
+    -- MarkDown preview
+    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 end)
